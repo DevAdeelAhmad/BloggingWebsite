b467ea27cdee04e91f53c981baf5c1e5f3b0ef95
DevAdeelAhmad <fa20-bse-031@cuilahore.edu.pk>
2023-06-29 23:36:13 +0500

    npm run dev

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const PORT = 8001;
 app.set("view engine" , "ejs");
 app.set("views", path.resolve("./views") )
 
+app.get("/", (req,res) => {
+    res.render("home");
+});
+
 app.listen(PORT, ()=> {
     console.log(`Server listening on port : ${PORT}`);
 });
