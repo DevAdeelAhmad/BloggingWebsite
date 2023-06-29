cbd25a40a3840a2f06eeb8225c57df9e1e125c45
DevAdeelAhmad <fa20-bse-031@cuilahore.edu.pk>
2023-06-29 23:54:59 +0500

    user model development

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImageURL: {
+      type: String,
+      default: "/images/defaultAvatar.png",
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("user", UserSchema);
+
+module.exports = User;
