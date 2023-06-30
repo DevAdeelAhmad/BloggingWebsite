dd60c32c3b4f4a48e1db4b02b03ad778e3444718
DevAdeelAhmad <fa20-bse-031@cuilahore.edu.pk>
2023-06-30 23:50:17 +0500

    installed cookie parser

---
+++ middlewares/authenticaion.js
@@ -0,0 +1,17 @@
+const { validateToken } = require("../services/authentication");
+function checkForAuthenticationCookie() {
+  return (req, res, next) => {
+    const tokenCookieValue = req.cookies[cookieName];
+    if (!tokenCookieValue) {
+      next();
+    }
+    try {
+      const userPayload = validateToken(tokenCookieValue);
+      req.user = userPayload;
+    } catch (error) {}
+    next();
+};
+}
+
+
+module.exports = {checkForAuthenticationCookie};
