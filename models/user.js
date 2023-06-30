8d834ff8db4e2948207471ee5932d1a512f2d761
DevAdeelAhmad <fa20-bse-031@cuilahore.edu.pk>
2023-06-30 15:35:52 +0500

    encrypting user passwords before saving them

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const { createHmac, randomBytes } = require("crypto");
 const { Schema, model } = require("mongoose");
 const userSchema = new Schema(
   {
@@ -30,6 +31,20 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return;
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+    this.salt = salt;
+    this.password = hashedPassword
+
+    next();
+});
 
 const User = model("user", UserSchema);
 
