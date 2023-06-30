b534339eeb7ff6364fb85fb506d09fb2a7e51192
DevAdeelAhmad <fa20-bse-031@cuilahore.edu.pk>
2023-06-30 15:37:30 +0500

    sign in and up routes

--- routes/user.js
+++ routes/user.js
@@ -2,4 +2,13 @@ const {Router} = require('express');
 
 const router = Router();
 
+router.get('/signin', (req, res) => {
+    return res.render('signin');
+});
+
+router.get('/signup', (req, res) => {
+    return res.render('signup');
+});
+
+
 module.exports = router;
