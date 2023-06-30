67cc45ad64700744f839f6622f072d84848b41a8
DevAdeelAhmad <fa20-bse-031@cuilahore.edu.pk>
2023-06-30 15:41:53 +0500

    signup post method handler

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,19 @@
-const {Router} = require('express');
-
+const { Router } = require("express");
+const User = require("../models/user");
 const router = Router();
 
-router.get('/signin', (req, res) => {
-    return res.render('signin');
+router.get("/signin", (req, res) => {
+  return res.render("signin");
 });
 
-router.get('/signup', (req, res) => {
-    return res.render('signup');
+router.get("/signup", (req, res) => {
+  return res.render("signup");
+});
+
+router.post("/signup", async (req, res) => {
+  const { fullName, email, password } = req.body;
+  await User.create({ fullName, email, password });
+  return res.redirect('/');
 });
 
 
